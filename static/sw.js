a9f4f02b7b622e4bf2e4d3d5faf39cc4e1b60304
luizgarro08-hub <luizgarro08@gmail.com>
2026-08-03 15:31:59 -0300

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('App instalado com sucesso!');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Mantém o app funcionando online com as rotas do Flask
+});
